1aee30dbfd7db199a4922b0e85b20b3c4a52a1f3
unknown <t043300520@unitru.edu.pe>
2024-06-06 22:38:37 -0500

    seccion3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { Section1Component } from './components/section-1/section-1.component';
 import { Section2Component } from './components/section-2/section-2.component';
+import { Section3Component } from './components/section-3/section-3.component';
+
 import { Section4Component } from './components/section-4/section-4.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ReactiveFormsModule , FormsModule} from '@angular/forms';
@@ -22,6 +24,7 @@ import { ReactiveFormsModule , FormsModule} from '@angular/forms';
     FormsModule,
     Section1Component,
     Section2Component,
+    Section3Component,
     Section4Component,
     FooterComponent,
     BrowserAnimationsModule,
